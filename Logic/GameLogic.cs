52d170f2d4080a42363ae3ae155f299f2df8bd2e
kubarni <kubarni@stud.uni-obuda.hu>
2022-03-29 20:32:36 +0200

    bomb progressbar done Display in progress IGameModel

--- Logic/GameLogic.cs
+++ Logic/GameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI_20212202_CM7A68.Models;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -10,13 +11,32 @@ namespace GUI_20212202_CM7A68.Logic
     public class GameLogic:IGameModel, IGameControl
     {
         Size area;
+        public List<Bomb> Bombs { get; set; }
         public void SetupSize(Size area)
         {
+            Bombs = new List<Bomb>();
             this.area = area;
         }
+        public void NewBomb()
+        {
+            Bombs.Add(new FallingBomb(new System.Drawing.Point(200, 550)));
+            Bombs.Add(new ThrowingBomb(new System.Drawing.Point(1000, 550), -1));
+            Bombs.Add(new ThrowingBomb(new System.Drawing.Point(400, 550), 1));
+
+        }
         public void TimeStep()
-        { 
-        //TODO: minden mozgatást, állapotváltozást, ütközést itt állítani, ez 20ms-ként le fog futni
+        {
+            //TODO: minden mozgatást, állapotváltozást, ütközést itt állítani, ez 20ms-ként le fog futni
+            for (int i = 0; i < Bombs.Count; i++)
+            {
+                Bombs[i].Move(600, new System.Drawing.Size((int)area.Width, (int)area.Height));
+                Bombs[i].Heal -= 1;
+                if (Bombs[i].Heal <= 0)
+                {
+                    Bombs.RemoveAt(i);
+                    //robbanás meghívása itt lehetséges lenne
+                }
+            }
         }
 
     }
--- Logic/IGameModel.cs
+++ Logic/IGameModel.cs
@@ -1,6 +1,11 @@
-﻿namespace GUI_20212202_CM7A68.Logic
+﻿using GUI_20212202_CM7A68.Models;
+using System.Collections.Generic;
+
+namespace GUI_20212202_CM7A68.Logic
 {
     public interface IGameModel
     {
+        List<Bomb> Bombs { get; set; }
+        void NewBomb();
     }
 }
