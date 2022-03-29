8f860aee9bc16a19e085d16c404631fba15a7768
kubarni <kubarni@stud.uni-obuda.hu>
2022-03-29 20:14:30 +0200

    Throwing bomb class done

--- Models/Bomb.cs
+++ Models/Bomb.cs
@@ -37,4 +37,89 @@ namespace GUI_20212202_CM7A68.Models
             }
         }
     }
+    internal class ThrowingBomb : Bomb
+    {
+        public ThrowingBomb(System.Drawing.Point center, int directionX)
+        {
+            Center = center;
+            this.direction = directionX;
+            x = 0;
+            Heal = 100;
+            Power = 20;
+            Speed = new Vector(4, 6);
+        }
+        double x;
+        //direction: melyik irányba kell dobni, 1-jobbra, (-1)-balra
+        int direction;
+
+
+        //TODO: pattogás a falról
+        public override void Move(int Floor, System.Drawing.Size area)
+        {
+            //jobbra
+            if (direction == 1)
+            {
+                if (Center.Y - Speed.Y <= Floor)
+                {
+                    //ennyit megy felfelé
+                    if (x <= Speed.X * 5)
+                    {
+                        x++;
+                        System.Drawing.Point newCenter_up =
+                                new System.Drawing.Point(Center.X + (int)Speed.X,
+                                Center.Y - (int)Speed.Y);
+                        Center = newCenter_up;
+                    }
+                    //vízszintes irány
+                    else if (x >= Speed.X * 5 && x <= Speed.X * 7)
+                    {
+                        x++;
+                        System.Drawing.Point newCenter_cons =
+                                new System.Drawing.Point(Center.X + (int)Speed.X,
+                                Center.Y);
+                        Center = newCenter_cons;
+                    }
+                    //zuhanás
+                    else
+                    {
+                        System.Drawing.Point newCenter_down = new System.Drawing.Point(Center.X + (int)Speed.X,
+                                    Center.Y + (int)Speed.Y);
+                        Center = newCenter_down;
+                    }
+                }
+            }
+            //balra
+            else if (direction == -1)
+            {
+                if (Center.Y - Speed.Y <= Floor)
+                {
+                    //felfelé
+                    if (x < Speed.X * 5)
+                    {
+                        x++;
+                        System.Drawing.Point newCenter_up =
+                                new System.Drawing.Point(Center.X - (int)Speed.X,
+                                Center.Y - (int)Speed.Y);
+                        Center = newCenter_up;
+                    }
+                    //vízszintes
+                    else if (x >= Speed.X * 5 && x <= Speed.X * 7)
+                    {
+                        x++;
+                        System.Drawing.Point newCenter_cons =
+                                new System.Drawing.Point(Center.X - (int)Speed.X,
+                                Center.Y);
+                        Center = newCenter_cons;
+                    }
+                    //lefelé
+                    else
+                    {
+                        System.Drawing.Point newCenter_down = new System.Drawing.Point(Center.X - (int)Speed.X,
+                                    Center.Y + (int)Speed.Y);
+                        Center = newCenter_down;
+                    }
+                }
+            }
+        }
+    }
 }
