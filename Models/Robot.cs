46e9e996b4a1a9b1aaacf7c522d6e9d1d6cd5bc2
hmarcell <marcellhanyecz@gmail.com>
2022-03-29 17:40:46 +0200

    temporary robot class added

---
+++ Models/Robot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GUI_20212202_CM7A68.Logic.GameLogic;
+
+namespace GUI_20212202_CM7A68.Models
+{
+    public class Robot      //TEMPORARY: Remove before merging
+    {
+        public bool IsControllable { get; set; }
+        public int Health { get; set; }
+        public int Shield { get; set; }
+        public Point Center { get; set; }
+        public Robot(Point spawnpoint)
+        {
+            this.Health = 100;
+            this.Shield = 100;
+            this.Center = spawnpoint;
+        }
+    }
+}
