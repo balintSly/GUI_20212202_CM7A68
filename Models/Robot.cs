98fb51108761f5cf12a684c5da4d1aa24e46e1b0
Sly <sziszko0129@gmail.com>
2022-03-31 20:57:40 +0200

    robots hp can't go below 0

--- Models/Robot.cs
+++ Models/Robot.cs
@@ -11,7 +11,25 @@ namespace GUI_20212202_CM7A68.Models
     public class Robot
     {
         public bool IsControllable { get; set; }
-        public int Health { get; set; }
+        private int health;
+
+        public int Health
+        {
+            get { return health; }
+            set 
+            {
+                if (value>0)
+                {
+                    health = value;
+                }
+                else
+                {
+                    health=0;
+                }
+               
+            }
+        }
+
         public int Shield { get; set; }
         public Point Center { get; set; }
         public Robot(Point spawnpoint)
