c604403b1bd393618f6aa6d470feb98d38fb078a
Sly <sziszko0129@gmail.com>
2022-04-02 18:58:58 +0200

    new game progress, error with image loading

---
+++ MenuWindows/ViewModels/MainMenuWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace GUI_20212202_CM7A68.MenuWindows.ViewModels
+{
+    public class MainMenuWindowViewModel
+    {
+        public List<string> Maps { get; set; }
+        public List<string> PlayerOneSkins { get; set; }
+        public List<string> PlayerTwoSkins { get; set; }
+        public MainMenuWindowViewModel()
+        {
+            var asd = Directory.GetCurrentDirectory();
+            var image= new BitmapImage(new Uri(Path.Combine(asd, "Renderer", "Images", "Backgrounds","mkmap1.jpg"), UriKind.RelativeOrAbsolute));
+            Maps = Directory.GetFiles(Path.Combine(asd,"Renderer", "Images", "Backgrounds"), "*.jpg").ToList();
+            PlayerOneSkins = Directory.GetFiles(Path.Combine(asd,"Renderer", "Images", "Robots"), "*.png").ToList();
+            PlayerTwoSkins = Directory.GetFiles(Path.Combine(asd,"Renderer", "Images", "Robots"), "*.png").ToList();
+            ;
+        }
+    }
+}
