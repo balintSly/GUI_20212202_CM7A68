48ec7c03073506df3f64b82c938b079b94809b30
Sly <sziszko0129@gmail.com>
2022-04-03 02:06:33 +0200

    new game bindings

--- MenuWindows/ViewModels/MainMenuWindowViewModel.cs
+++ MenuWindows/ViewModels/MainMenuWindowViewModel.cs
@@ -13,12 +13,20 @@ namespace GUI_20212202_CM7A68.MenuWindows.ViewModels
         public List<string> Maps { get; set; }
         public List<string> PlayerOneSkins { get; set; }
         public List<string> PlayerTwoSkins { get; set; }
+
+        public string PlayerOneName { get; set; }
+        public string PlayerTwoName { get; set; }
+        public string SelectedMapRoute { get; set; }
+        public string SelctedPlayerOneSkinRoute { get; set; }
+        public string SelectedPlayerTwoSkinRoute { get; set; }
         public MainMenuWindowViewModel()
         {
             var asd = Directory.GetCurrentDirectory();
             Maps = Directory.GetFiles(Path.Combine(asd,"Renderer", "Images", "Backgrounds"), "*.jpg").ToList();
             PlayerOneSkins = Directory.GetFiles(Path.Combine(asd,"Renderer", "Images", "Robots"), "*.png").ToList();
             PlayerTwoSkins = Directory.GetFiles(Path.Combine(asd,"Renderer", "Images", "Robots"), "*.png").ToList();
+            PlayerOneName = "PlayerOne";
+            PlayerTwoName = "PlayerTwo";
             ;
         }
     }
